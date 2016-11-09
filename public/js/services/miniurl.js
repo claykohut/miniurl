23f6e776775b094065be46a711617ebc0fca692b
Clay Kohut <claykohut@MacBook-Pro-2.local>
2016-11-09 16:52:28 -0500

    getting basic frontend structure in place

--- public/js/services/miniurl.js
+++ public/js/services/miniurl.js
@@ -1,8 +1,20 @@
 angular.module('api.miniUrl', [])
 .factory('miniUrl', function($http, $q){
 
+	var urlData = {
+		longUrl: '',
+		shortUrl: ''
+	}
+
+	var makeUrlMini = function(){
+		console.log('do it!')
+	}
+
     return {
-    	
+    	urlData: urlData,
+    	isLoading: false,
+    	hasError: false,
+    	makeUrlMini: makeUrlMini
     };
 
 });
